02c1de570dc73013eb1247f5d5dea8d781160445
ymcastellar <yoycas@hotmail.com>
2020-08-03 18:15:28 -0500

    edited spaces

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp
-
 file { '/tmp/holberton':
 ensure  => present,
 path    => '/tmp/holberton',
-node    => '0744',
+mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
 content => 'I love Puppet'
