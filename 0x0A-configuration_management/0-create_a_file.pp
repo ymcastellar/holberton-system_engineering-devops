79ec1f23a0084251afc2aecc7f048065f9963cef
ymcastellar <yoycas@hotmail.com>
2020-08-03 18:06:26 -0500

    added files for task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp
+
+file { '/tmp/holberton':
+ensure => 'present',
+path => '/tmp/holberton',
+node => '0744',
+owner => 'www-data',
+group => 'www-data',
+content => 'I love Puppet'
+}
