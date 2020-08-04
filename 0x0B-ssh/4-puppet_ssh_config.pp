46ccda0c6b7358a93b8642a6aa05993299bd933b
ymcastellar <yoycas@hotmail.com>
2020-08-04 18:12:11 -0500

    added file for task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# set up client SSH configuration file without typing a password.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+}
