bbf275a9cdde3c81d803c1add4accb2508f410b9
ymcastellar <yoycas@hotmail.com>
2020-09-14 16:01:25 -0500

    added main func

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,28 +6,29 @@ import requests
 import sys
 import json
 
-# get url by id:
-users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                     .format(sys.argv[1]))
-todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                    .format(sys.argv[1]))
+if __name__ == "__main__":
+    # get url by id:
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(sys.argv[1]))
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                        .format(sys.argv[1]))
 
-# covert get to json file
-List_user = users.json()
-List_todo = todo.json()
+    # covert get to json file
+    List_user = users.json()
+    List_todo = todo.json()
 
-task_cont = 0
-task_done = 0
-titles_done = []
+    task_cont = 0
+    task_done = 0
+    titles_done = []
 
-# run into key to take values
-for task in List_todo:
-    if task['completed'] is True:
-        task_done += 1
-        titles_done.append(task['title'])
-    task_cont += 1
+    # run into key to take values
+    for task in List_todo:
+        if task['completed'] is True:
+            task_done += 1
+            titles_done.append(task['title'])
+        task_cont += 1
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(List_user['name'], task_done, task_cont))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(List_user['name'], task_done, task_cont))
 
-print(*titles_done, sep='\n')
+    print(*titles_done, sep='\n')
