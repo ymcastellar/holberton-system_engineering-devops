7b37732374ff120777c42e53d250219ff13b1c92
ymcastellar <yoycas@hotmail.com>
2020-09-17 14:02:36 -0500

    fix url

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,8 +7,7 @@ def top_ten(subreddit):
     """request num of subs"""
     header = {"User-Agent": "ymcastellar"}
     req = requests.get('https://reddit.com/r/' + subreddit
-                       + '/hot.json?limit=10',
-                       headers=header)
+                       + '/hot.json?limit=10', headers=header)
 
     top = req.json().get('data', {}).get('children', None)
 
